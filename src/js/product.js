5d792e41fb0772eefea96829bbd2f06fe848121a
sskiles66 <stephentskiles@hotmail.com>
2023-09-28 11:49:21 -0600

    Team Activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,9 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
+import productDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  if (localStorage.getItem("so-cart") !== null) {
-    let list = getLocalStorage("so-cart");
-    list.push(product);
-    setLocalStorage("so-cart", list);
-  } else {
-    let list = [];
-    list.push(product);
-    setLocalStorage("so-cart", list);
-  }
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const productId = getParam("product");
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+productDetails(productId, ".product-detail");
+
+console.log(findProductById(productId));
---
+++ src/js/productDetails.mjs
@@ -0,0 +1,68 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+
+/*
+  <h3 id="productName"></h3>
+  <h2 class="divider" id="productNameWithoutBrand"></h2>
+  <img id="productImage" class="divider" src="" alt="" />
+  <p class="product-card__price" id="productFinalPrice"></p>
+  <p class="product__color" id="productColorName"></p>
+  <p class="product__description" id="productDescriptionHtmlSimple"></p>
+  <div class="product-detail__add">
+    <button id="addToCart" data-id="">Add to Cart</button>
+  </div>
+*/
+
+import { findProductById } from "./productData.mjs";
+
+let product = {};
+
+function productDetailsTemplate(product){
+    return `<h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${product.Image}"
+      alt="${product.Name}"
+    />
+    <p class="product-card__price">$${product.FinalPrice}</p>
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div>`;
+}
+
+
+
+export default async function productDetails(productId, selector){
+    // get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  product = await findProductById(productId);
+  // once we have the product details we can render out the HTML
+  const el = document.querySelector(selector);
+  el.insertAdjacentHTML("afterBegin", productDetailsTemplate(product));
+  // once the HTML is rendered we can add a listener to Add to Cart button
+  document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+}
+
+function addProductToCart(product) {
+    if (localStorage.getItem("so-cart") !== null) {
+      let list = getLocalStorage("so-cart");
+      list.push(product);
+      setLocalStorage("so-cart", list);
+    } else {
+      let list = [];
+      list.push(product);
+      setLocalStorage("so-cart", list);
+    }
+  }
+  // add to cart button event handler
+  async function addToCartHandler(e) {
+    const product = await findProductById(e.target.dataset.id);
+    addProductToCart(product);
+  }
+  
+
+
