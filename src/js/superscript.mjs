96041342b9f19b59b723acce220aec5cb50b9a11
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-04 21:10:13 -0600

    [fix]: Updated code to remove error on empty cart.
    The <sup> would try to get inserted and styled after the backpack icon, but there was no data in localStorage, resulting in an error. This commit addresses the issue
    
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- src/js/superscript.mjs
+++ src/js/superscript.mjs
@@ -6,20 +6,25 @@ const cartItems = JSON.parse(cartData);
 const cartIcon = document.querySelector(".cart");
 
 function createSuperscript() {
-  cartIcon.insertAdjacentHTML("beforeend", `<sup class="cart__superscript">${cartItems.length}</sup>`);
-  // Reference the superscript
-  const superscript = document.querySelector(".cart__superscript");
-  // Style the superscript
-  superscript.style.position = "absolute";
-  superscript.style.top = "4px";
-  superscript.style.right = "-8px";
-  superscript.style.backgroundColor = "red";
-  superscript.style.borderRadius = "50%";
-  superscript.style.padding = "1.3px";
-  if (cartItems.length < 9) {
-    superscript.style.padding = "0";
-    superscript.style.width = "16px";
-    superscript.style.height = "16px";
+  // This checks if there is data in localStorage
+  // This also removes error message in console
+  if (cartData !== null) {
+    cartIcon.insertAdjacentHTML("beforeend", `<sup class="cart__superscript">${cartItems.length}</sup>`);
+    // Reference the superscript
+    const superscript = document.querySelector(".cart__superscript");
+    // Style the superscript
+    superscript.style.position = "absolute";
+    superscript.style.top = "4px";
+    superscript.style.right = "-8px";
+    superscript.style.backgroundColor = "red";
+    superscript.style.borderRadius = "50%";
+    superscript.style.padding = "1.3px";
+    // Styles superscript when there are less than 9 items in cart
+    if (cartItems.length < 9) {
+      superscript.style.padding = "0";
+      superscript.style.width = "16px";
+      superscript.style.height = "16px";
+    }
   }
 }
 
@@ -27,16 +32,7 @@ function styleCart() {
   cartIcon.style.position = "relative";
 }
 
-function updateOnClick() {
-  // This code runs when localStorage is updated
-  window.addEventListener("storage", (e) => {
-    console.log(e.key);
-    console.log("storage event fired");
-  });
-}
-
 export default function displaySuperscript() {
   createSuperscript();
   styleCart();
-  // updateOnClick();
 }
