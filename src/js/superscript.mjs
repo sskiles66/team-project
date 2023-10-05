02d8ae7370efc645879a3ba1a848756f5787801c
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-04 19:33:31 -0600

    [fix]: Fix superscript width and height when cart items are less than 10 by giving them base width and height of 16px.
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- src/js/superscript.mjs
+++ src/js/superscript.mjs
@@ -11,18 +11,32 @@ function createSuperscript() {
   const superscript = document.querySelector(".cart__superscript");
   // Style the superscript
   superscript.style.position = "absolute";
-  superscript.style.top = "4px"
-  superscript.style.right = "-8px"
+  superscript.style.top = "4px";
+  superscript.style.right = "-8px";
   superscript.style.backgroundColor = "red";
   superscript.style.borderRadius = "50%";
-  superscript.style.padding = "2px";
+  superscript.style.padding = "1.3px";
+  if (cartItems.length < 9) {
+    superscript.style.padding = "0";
+    superscript.style.width = "16px";
+    superscript.style.height = "16px";
+  }
 }
 
 function styleCart() {
   cartIcon.style.position = "relative";
 }
 
+function updateOnClick() {
+  // This code runs when localStorage is updated
+  window.addEventListener("storage", (e) => {
+    console.log(e.key);
+    console.log("storage event fired");
+  });
+}
+
 export default function displaySuperscript() {
   createSuperscript();
   styleCart();
+  // updateOnClick();
 }
