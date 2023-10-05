69c9e767e69cba3c47d67b87fb1dbadaf3bafe10
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-04 22:23:33 -0600

    [feat]: Created updateCartSuperscript(). This function will update the superscript when addToCart button is clicked.
    This code uses the MutationObserver because the 'Add to Cart' button is asynchronous and would not work otherwise.
    
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- src/js/superscript.mjs
+++ src/js/superscript.mjs
@@ -32,7 +32,42 @@ function styleCart() {
   cartIcon.style.position = "relative";
 }
 
+// todo: Fix add to cart on empty cart
+function updateCartSuperscript() {
+// Check for button click
+// This checks if the DOM is loaded and will run code after it is loaded
+  const observer = new MutationObserver(() => {
+    // This will check if the button exists
+    if (document.querySelector("#addToCart")) {
+      const addToCartBtn = document.querySelector("#addToCart");
+
+      // Check if the event listener is already added to avoid duplicate executions
+      if (!addToCartBtn._clickListenerAdded) {
+        addToCartBtn.addEventListener("click", () => {
+          // Retrieve the current value .cart__superscript and turn string into int
+          const cartSuperscript = document.querySelector(".cart__superscript");
+          /* Why the 10? It's good practice and to make sure that the string is always interpreted as a decimal number
+          even if it will have no decimals. Just in case we get HEX or octal */
+          let currentValue = parseInt(cartSuperscript.textContent, 10);
+
+          // Increment the value
+          currentValue++;
+
+          // Update the cart superscript with new value
+          cartSuperscript.textContent = currentValue.toString();
+        });
+        // Set flag to check if the click event listener has been added
+        addToCartBtn._clickListenerAdded = true;
+      }
+      // observer.disconnect(); // Normally, we would disconnect, but we want this code block to keep running
+    }
+  });
+
+  observer.observe(document.documentElement, { childList: true, subtree: true });
+}
+
 export default function displaySuperscript() {
   createSuperscript();
   styleCart();
+  updateCartSuperscript();
 }
