d7235671e56ffe3b1d9fe9906b8ba2a35b5f91e1
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-05 19:44:10 -0600

    [feat]: Discount code to product listing
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

---
+++ src/js/components/DiscountedPrice.svelte
@@ -0,0 +1,8 @@
+<script>
+  // export let discountPercent = 10
+  //
+  // export function calculateDiscount(originalPrice, discountPercent) {
+  //   const discountPrice = originalPrice - (originalPrice * (discountPercent / 100))
+  //   return discountPrice.toFixed(2)
+  // }
+</script>
--- src/js/components/ProductList.svelte
+++ src/js/components/ProductList.svelte
@@ -1,34 +1,37 @@
 <script>
-    import { getData } from "../productData.mjs";
-    
-    // this is how we make a prop in svelte
-     // this is how we make a prop in svelte
-     export let category;
-    // if you are looking at this thinking that's strange to just stop with a promise
-        // you would be right.  This will make more sense in a bit...stay tuned.
-    // you would be right.  This will make more sense in a bit...stay tuned.
-    let promise = getData(category);
-    //console.log(promise.data)
+  import {getData} from "../productData.mjs";
 
-    const validIds = ["880RR", "985RF", "985PR","344YJ"];
+  export let discountPercent = 10
 
-    let filteredPromise = promise.then(data => {
-  return data.filter(product => validIds.includes(product.Id));
-    });
+  export function calculateDiscount(originalPrice, discountPercent) {
+    const discountPrice = originalPrice - (originalPrice * (discountPercent / 100))
+    return discountPrice.toFixed(2)
+  }
 
-    
+  // this is how we make a prop in svelte
+  export let category;
+  // if you are looking at this thinking that's strange to just stop with a promise
+  // you would be right.  This will make more sense in a bit...stay tuned.
+  let promise = getData(category);
+  //console.log(promise.data)
+
+  const validIds = ["880RR", "985RF", "985PR", "344YJ"];
+
+  let filteredPromise = promise.then(data => {
+    return data.filter(product => validIds.includes(product.Id));
+  });
 </script>
 
 <p>Top products: {category}</p>
 
 
 {#await filteredPromise}
-	<p>...waiting</p>
+  <p>...waiting</p>
 {:then data}
-    <ul class="product-list">
+  <ul class="product-list">
 
     {#each data as product}
-    <li class="product-card">
+      <li class="product-card">
         <a href="product_pages/index.html?product={product.Id}">
           <img
             src={product.Image}
@@ -36,13 +39,13 @@
           />
           <h3 class="card__brand">{product.Brand.Name}</h3>
           <h2 class="card__name">{product.Name}</h2>
-          <p class="product-card__price">{product.ListPrice}</p></a
-        >
+          <p class="product-card__price"><s>{product.ListPrice}</s> {calculateDiscount(product.ListPrice, discountPercent)}</p>
+        </a>
       </li>
     {/each}
-</ul>
+  </ul>
 
 {:catch error}
-	<p style="color: red">{error.message}</p>
+  <p style="color: red">{error.message}</p>
 {/await}
 
