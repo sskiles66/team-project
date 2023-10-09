0c4dd315f797aac4c6237a531fa5a8459c857fdb
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-09 17:10:14 -0600

    Made discountPercentage universal
    - Remove discountPercentage as props.
    - Move imports to top of file.
    
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- src/js/components/DiscountedPrice.svelte
+++ src/js/components/DiscountedPrice.svelte
@@ -1,8 +1,13 @@
 <script>
-  export let finalPrice;
-  export let discountPercentage;
-
+  // Import the 'calculateDiscount' function from the 'calculateDiscount.mjs' module.
   import {calculateDiscount} from "../calculateDiscount.mjs";
+
+  // Define a prop 'finalPrice' that will be passed into this component.
+  export let finalPrice;
+
+  // Define a constant 'discountPercentage' with a value of 18.
+  const discountPercentage = 18;
 </script>
 
+<!-- Display the result of calculating the discount using 'calculateDiscount' function. -->
 <p>${calculateDiscount(finalPrice, discountPercentage)}</p>
