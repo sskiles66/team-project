b4cbbba29a5822a4b93b1b2a454b9acf3c302cc0
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-09 16:43:58 -0600

    Cleaned up code
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,26 +1,7 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-
-
-
-
-/*
-  <h3 id="productName"></h3>
-  <h2 class="divider" id="productNameWithoutBrand"></h2>
-  <img id="productImage" class="divider" src="" alt="" />
-  <p class="product-card__price" id="productFinalPrice"></p>
-  <p class="product__color" id="productColorName"></p>
-  <p class="product__description" id="productDescriptionHtmlSimple"></p>
-  <div class="product-detail__add">
-    <button id="addToCart" data-id="">Add to Cart</button>
-  </div>
-*/
-
-
-import { findProductById } from "./productData.mjs";
-
+import {getLocalStorage, setLocalStorage} from "./utils.mjs";
+import {findProductById} from "./productData.mjs";
 
 let product = {};
-
 
 function productDetailsTemplate(product) {
   return `<h3>${product.Brand.Name}</h3>
@@ -40,21 +21,16 @@ function productDetailsTemplate(product) {
     </div>`;
 }
 
-
-
-
 export default async function productDetails(productId, selector) {
   // get the details for the current product. findProductById will return a promise! use await or .then() to process it
   product = await findProductById(productId);
 
-
-  if (product == undefined){
+  if (product === undefined) {
     alert("Error: Product not found!");
-  }else{
+  } else {
     // once we have the product details we can render out the HTML
     const el = document.querySelector(selector);
     el.insertAdjacentHTML("afterBegin", productDetailsTemplate(product));
-
 
     // This function will animate the cart when the user clicks on the Add to Cart button
     function animateCart() {
@@ -64,16 +40,13 @@ export default async function productDetails(productId, selector) {
       }, 180);
     }
 
-
     // once the HTML is rendered we can add a listener to Add to Cart button and animate the cart
     document.getElementById("addToCart").addEventListener("mousedown", (e) => {
       addToCartHandler(e);
       animateCart();
     });
   }
- 
 }
-
 
 function addProductToCart(product) {
   if (localStorage.getItem("so-cart") !== null) {
@@ -87,16 +60,8 @@ function addProductToCart(product) {
   }
 }
 
-
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
- 
-
-
-
-
-
-
