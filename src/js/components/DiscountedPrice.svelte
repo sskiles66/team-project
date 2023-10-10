5c5a862dfd3603b0b62a70e70a691e37343c1d98
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-09 18:20:24 -0600

    Remove const discountPercentage to calculateDiscount.mjs and updated parameters on line 10
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- src/js/components/DiscountedPrice.svelte
+++ src/js/components/DiscountedPrice.svelte
@@ -4,10 +4,7 @@
 
   // Define a prop 'finalPrice' that will be passed into this component.
   export let finalPrice;
-
-  // Define a constant 'discountPercentage' with a value of 18.
-  const discountPercentage = 18;
 </script>
 
 <!-- Display the result of calculating the discount using 'calculateDiscount' function. -->
-<p>${calculateDiscount(finalPrice, discountPercentage)}</p>
+<p>${calculateDiscount(finalPrice)}</p>
