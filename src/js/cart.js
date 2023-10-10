66c399481bef021d0ff75386d41f0aad951d66a0
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2023-10-09 20:54:15 -0600

    Add discount visually to cart page
    - This commit imports the calculateDiscount function to calculate the discount based off final price
    
    Signed-off-by: Abraham Jimenez <abraham.jimenez.romo@gmail.com>

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,5 @@
 import {getLocalStorage} from "./utils.mjs";
+import {calculateDiscount} from "./calculateDiscount.mjs";
 
 const totalSel = document.querySelector("#total");
 
@@ -17,7 +18,14 @@ function getTotalCost() {
   let total = 0;
   const cartItems = getLocalStorage("so-cart");
   cartItems.forEach((element) => {
+    /*
+    I feel like this line of code that I modified can cause Issues. Currently, this is only adding items that are
+    discounted. If an item has no discount, the item price will not be added to the total, I assume.
+    Here is the old line of code in order to add items without a discount:
     total += element.ListPrice;
+    Commit Hash: 0c83c499a3ae2aad2bdda36a8e20012a4380f612
+    */
+    total += parseFloat(calculateDiscount(element.ListPrice));
   });
 
   return total;
@@ -36,7 +44,10 @@ function cartItemTemplate(item) {
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
+  <div>
+    <s class="cart-card__price">$${item.FinalPrice}</s>
+    <p>$${calculateDiscount(item.FinalPrice)}</p>
+  </div>
 </li>`;
 }
 
