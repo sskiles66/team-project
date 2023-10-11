0cd63ef229170a5fb9d70ed450332b70f0d2f71a
sskiles66 <stephentskiles@hotmail.com>
2023-10-11 16:40:47 -0600

    Make quantities for cart items

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,10 +2,33 @@ import { getLocalStorage } from "./utils.mjs";
 
 const totalSel = document.querySelector("#total");
 
+let products = [];
+
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   if (cartItems !== null) {
-    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    // cartItems.forEach((item) => {
+    //   if (!products.some(item)){
+    //     products.push(item)
+    //   }
+    //   console.log(products);
+    // })
+    //products = cartItems;
+
+    cartItems.forEach((item) => {
+      let existingItem = products.find((product) => product.Id === item.Id);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        item.quantity = 1;
+        products.push(item);
+      }
+    });
+
+    console.log(cartItems);
+    console.log(products);
+
+    const htmlItems = products.map((item) => cartItemTemplate(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
     totalSel.classList.remove("hide");
     const total = getTotalCost();
@@ -35,7 +58,7 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__quantity">qty: ${item.quantity}</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 
