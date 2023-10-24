bdb626f4b8cb76c299d38d8e0bddf2766f023d27
sskiles66 <stephentskiles@hotmail.com>
2023-10-24 10:34:09 -0600

    made changes hopefully fix problems

--- src/js/form.js
+++ src/js/form.js
@@ -1,8 +1,8 @@
-button = document.getElementById("news-form");
-button.addEventListener('submit', function(event){
-    event.preventDefault(); // Prevent the default form submission
+const button = document.getElementById("news-form");
+button.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission
 
-     // Replace the form with the "thank you" message
-    document.getElementById("news-form").style.display = "none";
-    document.getElementById("thankYouMessage").style.display = "block";
+  // Replace the form with the "thank you" message
+  document.getElementById("news-form").style.display = "none";
+  document.getElementById("thankYouMessage").style.display = "block";
 });
