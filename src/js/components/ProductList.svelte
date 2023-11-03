df3ab1c8e1318a16c5272dc879b87e5d7ce3de3a
sskiles66 <stephentskiles@hotmail.com>
2023-11-02 20:09:19 -0600

    Finished Trello Card

--- src/js/components/ProductList.svelte
+++ src/js/components/ProductList.svelte
@@ -1,14 +1,31 @@
 <script>
   import {getProductsByCategory} from "../externalServices.mjs";
+  import { getLocalStorage, setLocalStorage } from "../utils.mjs";
   import DiscountedPrice from "./DiscountedPrice.svelte";
 
   // this is how we make a prop in svelte
   export let category;
   // if you are looking at this thinking that's strange to just stop with a promise
   // you would be right.  This will make more sense in a bit...stay tuned.
+
+
   let promise = getProductsByCategory(category);
 
   let value = localStorage.getItem("searchValue")
+
+  let min = getLocalStorage("min");
+  let max = getLocalStorage("max");
+
+  if (min == null || max == null){
+    min.value = 0;
+    max.value = 200;
+    setLocalStorage("min", 0);
+    setLocalStorage("max", 200);
+  }
+
+  console.log(min);
+  console.log(max);
+
 
   console.log(value);
   //console.log(promise.data)
@@ -16,9 +33,19 @@
   let filteredData = []
 
   promise.then(data => {
-  filteredData = data.filter(product => product.Name.includes(value));
-  console.log(filteredData);
+  filteredData = data.filter(product => 
+    product.Name.includes(value) &&
+    product.FinalPrice >= min && // Minimum price
+    product.FinalPrice <= max // Maximum price
+  );
 });
+
+  console.log(filteredData);
+  
+
+ 
+
+
 
   function openModal(product){
     console.log(product);
@@ -34,7 +61,7 @@
 
 </script>
 
-<p>Top products: {category}</p>
+<p>Top products: {category} between ${getLocalStorage("min")} and ${getLocalStorage("max")}</p>
 
 {#await filteredData}
   <p>...waiting</p>
@@ -62,13 +89,13 @@
   </ul>
   <div id="modal">
     <div id="modal-content">
-      <button on:click={closeModal} class="close">&times;</button>
+      <button on:click={closeModal} class="close-prod-list">&times;</button>
       <p id="modal-text">test for now</p>
       <img id="modal-img" src="#" alt="">
     </div>
   </div>
   {:else}
-  <p style="color: red">Sorry, there are no items with {value}. Try resetting the value in the search bar</p>
+  <p style="color: red">Sorry, there are no items with the current settings. Try resetting the value in the search bar or adjust the range of price.</p>
   {/if}
 
 {:catch error}
